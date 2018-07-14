1e4c8804a7f7f35f9736d36c5033afcf458373d4
Оксана Ткаченко <oksana@otbook.local>
2018-07-15 00:19:57 +0300

    added block comments to all posts, but need to change

--- src/index.js
+++ src/index.js
@@ -9,8 +9,24 @@ $(document).ready(function() {
  $('.grid').masonry({
     itemSelector: '.grid-item',
     columnWidth: '.grid-sizer',
-    //gutter: 10,
     percentPosition: true
   });
 });
 
+
+/**
+*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+/*
+var disqus_config = function () {
+this.page.url = 'https://coatceatl.github.io/blog/';  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = 'test'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://test-qfkhy4xl8a.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+*/
