1eb24db786360f6d02b5dc170662bccbdda8283c
Оксана Ткаченко <oksana@otbook.local>
2018-07-16 13:36:45 +0300

    added fadein and fadeout for nav-social

--- src/index.js
+++ src/index.js
@@ -6,11 +6,21 @@ import jQueryBridget from 'jquery-bridget';
 jQueryBridget('masonry', Masonry, $);
 
 $(document).ready(function() {
- $('.grid').masonry({
+  $('.grid').masonry({
     itemSelector: '.grid-item',
     columnWidth: '.grid-sizer',
     gutter:5,
     percentPosition: true
+  });
+
+  $('.navbar-toggler').on('click', function(e) {
+    console.log('start');
+    e.preventDefault();
+    $(this).toggleClass('active');
+  });
+
+  $('.navbar-toggler').click(function(){
+    $('.nav-social').fadeToggle();
   });
 });
 
